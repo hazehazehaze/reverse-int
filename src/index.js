3ffa086adf97a6c90d98260104371a1ba5a74c49
Ekaterina Mashkovich <katherinamashkovich@gmail.com>
2022-06-30 23:10:14 +0300

    feat: add algorithm

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let x = '';
+    n = String(n);
+    let lengthX = n.length;
+
+    for (let i = 0; i < n.length; i++) {
+        if (n[lengthX - 1] != '-') {
+            x = x + n[lengthX - 1];
+            lengthX--;
+        }
+
+    }
+
+    return x;
 }
